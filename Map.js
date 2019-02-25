511dfa78548b4d86b2b98b3411a121ba4d32696f
Sorokin V.V <sorokin752@mail.ru>
2019-02-26 02:17:18 +0300

    карта, палки, два окна

--- Map.js
+++ Map.js
@@ -1,9 +1,11 @@
 var modal = document.getElementById("Modal_Reg");
 var btn = document.getElementById("Reg");
 var span = document.getElementsByClassName("close")[0];
+
 btn.onclick = function(){
 	modal.style.display = "block"
 }
+
 span.onclick = function(){
 	modal.style.display = "none"
 }
